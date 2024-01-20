6f37584c9d817786fdb6e024df28e0dd408b8b26
Rafael Ramalho <rafazelramalho19@gmail.com>
2024-01-20 21:57:05 +0100

    feat: add reset title update

--- src/pages/api/reset.ts
+++ src/pages/api/reset.ts
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from '@/prisma-client';
+import { getTitle, sendMessage } from '@/twitch/twitch';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  success: string
+  success: string;
+  twitch?: boolean;
 }
 
 export default async function handler(
@@ -19,5 +21,21 @@ export default async function handler(
     }
   });
 
-  return res.status(200).json({ success: 'OK' })
+  if (!process.env.TWITCH_TOKEN) {
+    return res.status(200).json({ success: 'OK', twitch: false })
+  }
+
+  const title = await getTitle();
+
+  if (!title) {
+    return res.status(200).json({ success: 'OK', twitch: false })
+  }
+
+  const newTitle = title.replaceAll(/[0-9]\/5/g, `${nuke.number}/5`);
+
+  if (newTitle) {
+    await sendMessage(`!setTitle ${newTitle}`)
+  }
+
+  return res.status(200).json({ success: 'OK', twitch: true })
 }
